845bcc16820acf1e19e32612d373827abd4a6a7d
V. Kutsarit <kutsarit@web.de>
2017-01-15 05:36:24 +0100

    u

---
+++ Crow.Graphics.OpenGl/IHandleResolver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IHandleResolver
+{
+	void LoadHandle<T>(string name, out T result) where T : class;
+}
